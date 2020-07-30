e6d6f9cbbe9d1ba105dffb9bd3a3300ab3c1cb7a
Rai Medeiros <raismedeiros@gmail.com>
2020-07-29 22:03:57 -0300

    upload de arquivo CSV

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import uploadConfig from '../config/upload';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const upload = multer(uploadConfig);
 
 const transactionsRouter = Router();
 
@@ -39,10 +43,17 @@ transactionsRouter.delete('/:id', async (request, response) => {
 
   return response.json({ message: id });
 });
-/*
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-}); */
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactionsService = new ImportTransactionsService();
+    const transactions = await importTransactionsService.execute(
+      request.file.path,
+    );
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
